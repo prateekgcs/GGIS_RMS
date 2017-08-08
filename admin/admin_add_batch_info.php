6ecb55404cbbfacf237fd6c385baf48e32252362
Prateek Gupta <30597387+prateekgcs@users.noreply.github.com>
2017-08-08 20:32:48 +0530

    Upload File

--- admin/admin_add_batch_info.php
+++ admin/admin_add_batch_info.php
@@ -196,7 +196,7 @@
 						
 						header('Content-Description: File Transfer');
 						header('Content-Type: application/octet-stream');
-						header('Content-Disposition: attachment; filename=CLASS '.$clas.' ('.$year.')_INFO');
+						header('Content-Disposition: attachment; filename=CLASS '.$clas.' ('.$year.')_INFO.xlsx');
 						header('Expires: 0');
 						header('Cache-Control: must-revalidate');
 						header('Pragma: public');
